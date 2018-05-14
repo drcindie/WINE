b8bf58c7e327cbaba7325772ffdfea882035fa09
Cindie Marren <DrCindie@gmail.com>
2018-05-14 15:01:24 -0600

    none

---
+++ wine_data_list/src/main/java/com/cindiemarren/wine_data_list/wine_data_list.java
@@ -0,0 +1,13 @@
+package com.cindiemarren.wine_data_list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class wine_data_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wine_data_list);
+    }
+}
