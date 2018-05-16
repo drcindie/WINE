81bd7533c322037e7c306bdf5c8418391249584e
Cindie Marren <DrCindie@gmail.com>
2018-05-16 16:00:25 -0600

    ugh, had to backup. Can't get search to work

--- app/src/main/java/com/cindiemarren/wine_app/RedActivity.java
+++ app/src/main/java/com/cindiemarren/wine_app/RedActivity.java
@@ -6,11 +6,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RedActivity extends AppCompatActivity {
     MaterialSearchView searchView;
+    ListView firstView;
+
+    String[] firstSource = {
+
+            "Cindie",
+            "John",
+            "Cooper",
+            "Ivan",
+            "Princess",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+            "ten",
+            "eleven",
+            "twelve"
+    };
+//----------------ENDED VIDEO AT 14:38-------------------
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +50,12 @@ public class RedActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Wine Search");
         toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
 
-        searchView = findViewById(R.id.search_view);
+        searchView = (MaterialSearchView)findViewById(R.id.search_view);
+        firstView = (ListView)findViewById(R.id.firstView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,firstSource);
+        firstView.setAdapter(adapter);
 
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.redwinedata,menu);
@@ -33,5 +63,8 @@ public class RedActivity extends AppCompatActivity {
         searchView.setMenuItem(item);
         return true;
 
+            }
+
+
+
     }
-}
