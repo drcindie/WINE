fb3d7cb938c2c82de067b5f8cc06eaa4a9c437af
Cindie Marren <DrCindie@gmail.com>
2018-05-23 11:35:46 -0600

    update after major screw ups; remove gradle items

--- app/src/main/java/com/cindiemarren/wine_app/OtherListActivity.java
+++ app/src/main/java/com/cindiemarren/wine_app/OtherListActivity.java
@@ -2,19 +2,6 @@ package com.cindiemarren.wine_app;
 
 public class OtherListActivity {
 
-    private String wine;
-    private String description;
 
-    public OtherListActivity(String wine, String description) {
-        this.wine = wine;
-        this.description = description;
     }
 
-    public String getWine() {
-        return wine;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-}
